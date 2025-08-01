cf89de5f8e66dfaf07ce1f77495aa5e0283e20ac
CadmiumBeast <shakeelshajahan212@gmail.com>
2025-08-01 12:17:54 +0530

    Rooms

---
+++ app/Models/Room.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    //
+    protected $fillable = [
+        'RoomID',
+        'building_id', // Foreign key to locations table
+        'level', // Default level is 1
+        'venue_type_id', // Foreign key to venue_types table
+        'capacity', // Default capacity is 0
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Location::class, 'building_id');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class, 'building_id');
+    }
+    public function venueType()
+    {
+        return $this->belongsTo(VenueType::class, 'venue_type_id');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Location;
+use App\Models\Room;
+use App\Models\VenueType;
 
 class DatabaseSeeder extends Seeder
 {
@@ -44,6 +47,38 @@ class DatabaseSeeder extends Seeder
 
         foreach ($users as $user) {
             User::create($user);
+        }
+
+        $locations = [
+            ['name' => 'City Campus', 'levels' => 5],
+            ['name' => 'Law School', 'levels' => 3],
+            ['name' => 'Kandy Campus', 'levels' => 2],
+        ];
+
+        foreach ($locations as $location) {
+            Location::create($location);
+        }
+
+        $venueTypes = [
+            ['name' => 'Lecture Hall'],
+            ['name' => 'Computer Lab'],
+            ['name' => 'Conference Room'],
+            ['name' => 'Auditorium'],
+        ];
+
+        foreach ($venueTypes as $venueType) {
+            VenueType::create($venueType);
+        }
+
+        $rooms = [
+            ['RoomID' => 'LH101', 'building_id' => 1, 'level' => '1', 'venue_type_id' => 1, 'capacity' => 100],
+            ['RoomID' => 'CL201', 'building_id' => 2, 'level' => '2', 'venue_type_id' => 2, 'capacity' => 50],
+            ['RoomID' => 'CR301', 'building_id' => 3, 'level' => '3', 'venue_type_id' => 3, 'capacity' => 30],
+            ['RoomID' => 'AU401', 'building_id' => 1, 'level' => '4', 'venue_type_id' => 4, 'capacity' => 200],
+        ];
+
+        foreach ($rooms as $room) {
+            Room::create($room);
         }
     }
 }
